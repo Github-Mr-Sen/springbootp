71a67f687c0644067289aa130c47eadae094c814
张在森 <1165674854@qq.com>
2020-03-25 12:10:08 +0800

    统一返回数据的格式 1、创建一个新工程common 2、封装返回信息。里面用到了链式变成（新知识点）

---
+++ online/common/src/main/java/com/zsxk/online/common/Result.java
@@ -0,0 +1,62 @@
+package com.zsxk.online.common;
+
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+public class Result {
+
+    private boolean success;
+    private Integer code;
+    private String message;
+    private Map<String,Object> data = new HashMap<>();
+
+
+
+    private Result(){}
+    public static Result ok(){
+        Result r = new Result();
+        r.setSuccess(true);
+        r.setCode(StatusVO.success);
+        r.setMessage("操作成功");
+        return r;
+    }
+    public static Result error(){
+        Result r = new Result();
+        r.setSuccess(false);
+        r.setCode(StatusVO.err);
+        r.setMessage("操作失败");
+        return r;
+    }
+//    public static Result setResult(StatusVO statusVO){
+//        Result r = new Result();
+//        r.setSuccess(resultCodeEnum.getSuccess());
+//        r.setCode(resultCodeEnum.getCode());
+//        r.setMessage(resultCodeEnum.getMessage());
+//        return r;
+//    }
+    public Result success(Boolean success){
+        this.setSuccess(success);
+        return this;
+    }
+    public Result message(String message){
+        this.setMessage(message);
+        return this;
+    }
+    public Result code(Integer code){
+        this.setCode(code);
+        return this;
+    }
+    public Result data(String key, Object value){
+        this.data.put(key, value);
+        return this;
+    }
+    public Result data(Map<String, Object> map){
+        this.setData(map);
+        return this;
+    }
+
+
+}
---
+++ online/common/src/main/java/com/zsxk/online/common/StatusVO.java
@@ -0,0 +1,16 @@
+package com.zsxk.online.common;
+
+public interface StatusVO {
+    /*
+    * 成功状态码
+    * */
+     int success =10000;
+     /**
+      * 没有操作权限
+      * */
+     int author = 30000;
+    /**
+     * 失败状态码
+     * */
+    int err = 10001;
+}
