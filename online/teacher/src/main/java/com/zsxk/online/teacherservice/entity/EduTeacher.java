0455b4ebbba2154dabe9ca2c36380654cb9dc374
张在森 <1165674854@qq.com>
2020-03-25 10:20:14 +0800

    #配置时间的格式 spring.jackson.date-format=yyyy-MM-dd HH:mm:ss spring.jackson.time-zone=GMT+8 在配置文件中加入上述配置，统一时间格式

--- online/teacher/src/main/java/com/zsxk/online/teacherservice/entity/EduTeacher.java
+++ online/teacher/src/main/java/com/zsxk/online/teacherservice/entity/EduTeacher.java
@@ -9,6 +9,8 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
 
 /**
  * <p>
@@ -18,6 +20,7 @@ import lombok.experimental.Accessors;
  * @author 张在森
  * @since 2020-03-25
  */
+@Validated
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
@@ -51,9 +54,11 @@ public class EduTeacher implements Serializable {
     @ApiModelProperty(value = "逻辑删除 1（true）已删除， 0（false）未删除")
     private Boolean isDeleted;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @ApiModelProperty(value = "创建时间")
     private Date gmtCreate;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @ApiModelProperty(value = "更新时间")
     private Date gmtModified;
 
